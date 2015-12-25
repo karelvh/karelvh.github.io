a3babd4683f4236094ce25bdbedd444f17c6d54c
karelvh <karelvanhaeverbeek@gmail.com>
2015-12-25 13:17:51 +0100

    nightmode on hi page 🌙

--- hi/hi.js
+++ hi/hi.js
@@ -1,33 +1,25 @@
 var nightmodeEnabled = 0;
-var clock = document.getElementById('clock');
-
+var currentTime = new Date().getHours();
 document.addEventListener("keypress", nightmode);
 
 function nightmode(event) {
    var keyCode = event.which || event.keyCode;
-   // if 'n' (night) is pressed
    if (keyCode === 110 && nightmodeEnabled === 0) {
-      document.body.style.backgroundColor = "#202b37";
-      clock.style.color = "#F3F3F2";
+      document.body.style.backgroundColor = "#ef4723";
       nightmodeEnabled = 1;
-      // console.log(nightmodeEnabled);
    }
    else if (keyCode === 110 && nightmodeEnabled === 1) {
       document.body.style.backgroundColor = "#ffffff";
-      clock.style.color = "#202b37";
       nightmodeEnabled = 0;
-      // console.log(nightmodeEnabled);
    }
 }
 
 if (7 <= currentTime && currentTime < 19) {
    document.body.style.backgroundColor = "#ffffff";
-   clock.style.color = "#202b37";
    nightmodeEnabled = 0;
 }
 else {
-   document.body.style.backgroundColor = "#202b37";
-   clock.style.color = "#F3F3F2";
+   document.body.style.backgroundColor = "#ef4723";
    nightmodeEnabled = 1;
    // console.log(nightmodeEnabled);
 }
