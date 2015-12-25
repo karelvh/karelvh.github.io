431a3998ddd33f0824a66a9df3f30b4389af8949
karelvh <karelvanhaeverbeek@gmail.com>
2015-12-25 13:50:08 +0100

    structure 🏢
    Added some more structure to make it easier to switch out landing pages
    in the future. Every page will get it's own folder. Making it easier to
    remove or add to.

--- gulpfile.js
+++ gulpfile.js
@@ -12,15 +12,27 @@ var gulp = require("gulp"),
    autoprefixer = require("gulp-autoprefixer");
 
 gulp.task("default", function(){
-   var styleWatcher = gulp.watch("./css/*.scss", ["css"]);
+   var styleWatcher = gulp.watch("./index/css/*.scss", ["css"]);
    styleWatcher.on("change", function(event){});
 
-   var jsWatcher = gulp.watch("./js/*.js", ["js"]);
+   var jsWatcher = gulp.watch("./index/js/*.js", ["js"]);
    jsWatcher.on("change", function(){});
 });
 
-gulp.task("js", function(){
-   gulp.src("./js/*.js")
+//////////////////////////////
+//////index page tasks ///////
+//////////////////////////////
+
+gulp.task("index", function(){
+   var styleWatcher = gulp.watch("./index/css/*.scss", ["index-css"]);
+   styleWatcher.on("change", function(event){});
+
+   var jsWatcher = gulp.watch("./index/js/*.js", ["index-js"]);
+   jsWatcher.on("change", function(){});
+});
+
+gulp.task("index-js", function(){
+   gulp.src("./index/js/*.js")
       //.pipe(jshint())
       //.pipe(jshint.reporter(jsStylish))
       .pipe(sourcemaps.init())
@@ -32,8 +44,8 @@ gulp.task("js", function(){
       .pipe(notify({message: 'js built'}));
 });
 
-gulp.task("css", function () {
-   gulp.src("./css/*.scss")
+gulp.task("index-css", function () {
+   gulp.src("./index/css/*.scss")
       .pipe(sass().on('error', sass.logError))
       .pipe(csslint({
          'ids': false
@@ -41,10 +53,52 @@ gulp.task("css", function () {
       .pipe(csslint.reporter())
       .pipe(sourcemaps.init())
       .pipe(autoprefixer())
-      .pipe(concat("main.min.css"))
+      .pipe(concat("app.min.css"))
       .pipe(sourcemaps.write())
       //enable minifier before deployment
       .pipe(cssMinifier())
       .pipe(gulp.dest("./dist/css"))
+      .pipe(notify({message: 'stylesheet built'}));
+});
+
+//////////////////////////////
+/////// hi page tasks ////////
+//////////////////////////////
+
+gulp.task("hi", function(){
+   var styleWatcher = gulp.watch("./hi/css/*.scss", ["hi-css"]);
+   styleWatcher.on("change", function(event){});
+
+   var jsWatcher = gulp.watch("./hi/js/*.js", ["hi-js"]);
+   jsWatcher.on("change", function(){});
+});
+
+gulp.task("hi-js", function(){
+   gulp.src("./hi/js/*.js")
+      //.pipe(jshint())
+      //.pipe(jshint.reporter(jsStylish))
+      .pipe(sourcemaps.init())
+      .pipe(concat("app.min.js"))
+      .pipe(sourcemaps.write())
+      //enable uglify before deployment
+      .pipe(uglify())
+      .pipe(gulp.dest('./hi/dist/js'))
+      .pipe(notify({message: 'js built'}));
+});
+
+gulp.task("hi-css", function () {
+   gulp.src("./hi/css/*.scss")
+      .pipe(sass().on('error', sass.logError))
+      .pipe(csslint({
+         'ids': false
+      }))
+      .pipe(csslint.reporter())
+      .pipe(sourcemaps.init())
+      .pipe(autoprefixer())
+      .pipe(concat("app.min.css"))
+      .pipe(sourcemaps.write())
+      //enable minifier before deployment
+      .pipe(cssMinifier())
+      .pipe(gulp.dest("./hi/dist/css"))
       .pipe(notify({message: 'stylesheet built'}));
 });
