4d7b614d205e4fc0b794a747a392ed62b8a6096a
karelvh <karelvanhaeverbeek@gmail.com>
2016-01-04 12:24:38 +0100

    MAJOR 🔑 ALERT
    THEY DON'T WANT YOU TO UPDATE YOUR HTMLS.
    DJ Khaled has invaded the source code.

--- hi/js/hi.js
+++ hi/js/hi.js
@@ -23,3 +23,33 @@ function nightmode(event) {
 //    nightmodeEnabled = 1;
 //    // console.log(nightmodeEnabled);
 // }
+
+function myCallback() {
+    var salute;
+    var h = currentTime;
+    var coffee = '<img class="emoji" title="coffee" alt="coffee" src="https://github.global.ssl.fastly.net/images/icons/emoji/coffee.png" height="20" width="20" align="absmiddle">';
+    var sunglasses = '<img class="emoji" title="sunglasses" alt="sunglasses" src="https://github.global.ssl.fastly.net/images/icons/emoji/sunglasses.png" height="20" width="20" align="absmiddle">';
+    var moon = '<img class="emoji" title="moon" alt="moon" src="https://github.global.ssl.fastly.net/images/icons/emoji/crescent_moon.png" height="20" width="20" align="absmiddle">';
+    var star = '<img class="emoji" title="star" alt="star" src="https://github.global.ssl.fastly.net/images/icons/emoji/star2.png" height="20" width="20" align="absmiddle">';
+
+    // console.log(h);
+    if(h >= 3 && h < 12){
+        salute = "Good Morning " + coffee;
+        // console.log("Morning");
+    }
+    else if (h >= 12 && h < 18){
+        salute = "Good Afternoon " + sunglasses;
+        // console.log("Afternoon");
+    }
+    else if (h >= 18 && h < 22){
+        salute = "Good Evening "+ moon;
+        // console.log("Evening");
+    }
+    else{
+        salute = "Good Night "+ star;
+        // console.log("Night");
+    }
+    document.getElementById('emojiID').innerHTML = salute;
+}
+
+myCallback();
