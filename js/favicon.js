6b2ec5347e59843fd1e2cc1c725a7c6c2c2269a7
Karel Van Haeverbeek <karelvanhaeverbeek@gmail.com>
2017-02-06 19:35:43 +0100

    change of favicon

--- js/favicon.js
+++ js/favicon.js
@@ -1,5 +1,5 @@
 const $favicon = document.querySelector(`#favicon`);
 document.addEventListener(`visibilitychange`, function() {
   // document.title = document.hidden ? newTitle : title;
-  document.hidden ? $favicon.href = `./img/favicon-costanza-gray.png` : $favicon.href = `./img/favicon-costanza.png`;
+  document.hidden ? $favicon.href = `./img/favicon-costanza-gray.ico` : $favicon.href = `./img/favicon-costanza.ico`;
 });
