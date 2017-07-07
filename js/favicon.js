b5efa206bafad83fd8df35c43a84ee9ab65b267d
Karel Van Haeverbeek <karelvanhaeverbeek@gmail.com>
2017-07-07 12:06:11 +0200

    cleanup

--- js/favicon.js
+++
@@ -1,5 +0,0 @@
-const $favicon = document.querySelector(`#favicon`);
-document.addEventListener(`visibilitychange`, function() {
-  // document.title = document.hidden ? newTitle : title;
-  document.hidden ? $favicon.href = `./img/favicon-costanza-gray.png` : $favicon.href = `./img/favicon-costanza.png`;
-});
--- js/log.js
+++ js/log.js
@@ -1,2 +1,1 @@
-console.log('-> Do you want to see the code that powers this site? https://github.com/karelvh/karelvh.github.io');
-console.log('-> Big thanks to http://designersblock.co/ for the color scheme.');
+console.log('-> Do you want to see the code that powers this site? It\'s available on https://github.com/karelvh/karelvh.github.io');
