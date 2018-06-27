640e736cc3ad59c51b741f4055f066f45b21ff11
Karel Van Haeverbeek <karelvanhaeverbeek@gmail.com>
2018-06-27 16:44:08 +0200

    redesign

--- src/js/log.js
+++ src/js/log.js
@@ -1,4 +1,3 @@
 export default () => {
   console.log('-> Do you want to see the code that powers this site? https://github.com/karelvh/karelvh.github.io');
-  console.log('-> Big thanks to http://designersblock.co/ for the color scheme.');
 }
