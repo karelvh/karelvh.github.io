ae631c8a95aa50da4cfe897b6d7498ffe717b8a0
Karel Van Haeverbeek <karelvanhaeverbeek@gmail.com>
2018-09-10 18:15:55 +0200

    google verification

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
 import logger from './log';
 import favicon from './favicon';
-import VanillaTilt from 'vanilla-tilt';
+// import VanillaTilt from 'vanilla-tilt';
 
 logger();
 favicon();
-VanillaTilt.init(document.querySelector(`.content`), {
-    max: 3,
-});
+// VanillaTilt.init(document.querySelector(`.content`), {
+//     max: 3,
+// });
